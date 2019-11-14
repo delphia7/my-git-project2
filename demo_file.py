f74e25466b4c8949098ac0709db927c544cd1e23
delphia7 <rosendolo78@gmail.com>
2019-11-14 01:04:44 -0500

    Reverted back to 39510f4f766517612538df3534c5f861bdcb7329 for my_file.py

---
+++ demo_file.py
@@ -0,0 +1,11 @@
+print('Hi Git')
+
+print('Hi from another dev')
+
+def hi_there():
+    print("hi")
+
+    #something
+
+    def hi_there():
+    print("Update python function with merge conflict")
